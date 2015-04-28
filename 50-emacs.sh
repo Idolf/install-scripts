3a34c51a5b9f4039bdd47959c24a6984377d31b3
Mathias Svensson <idolf@pwnies.dk>
2015-04-28 23:33:01 +0200

    Emacs update

--- 50-emacs.sh
+++ 50-emacs.sh
@@ -2,8 +2,4 @@
 . "$(dirname "$0")/util.sh"
 
 require emacs emacs-goodies-el
-
-if [ ! -d .emacs.d/elpa ] && \
-       promptyn "Install Emacs packages now?" "y" ; then
-    run .emacs.d/install-packages.sh
-fi
+run .emacs.d/install-packages.sh
