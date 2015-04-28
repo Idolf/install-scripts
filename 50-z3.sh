9a95520a980f2af27ce11e6f1baca3e29d2c444d
Mathias Svensson <idolf@pwnies.dk>
2015-04-28 11:59:59 +0200

    .

--- 50-z3.sh
+++ 50-z3.sh
@@ -4,12 +4,11 @@ prologue
 
 sudo rm -rf /opt/z3
 sudo mkdir /opt/z3
-sudo chown `id -u`:`id -g` /opt/z3
 
 git clone git@github.com:Z3Prover/z3.git .
-python scripts/mk_make.py --prefix=/opt/z3
+sudo python scripts/mk_make.py --prefix=/opt/z3
 cd build
-make
+sudo make
 sudo make install
 
 epilogue
