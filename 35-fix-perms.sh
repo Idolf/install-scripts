33fb66a006f2db1a1c224d96ffdba2be47e9fd95
Mathias Svensson <idolf@pwnies.dk>
2015-04-29 01:16:04 +0200

    More permission fixes

---
+++ 35-fix-perms.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+. "$(dirname "$0")/util.sh"
+
+run chmod og-rwx .ssh .ssh/id_rsa
+run sudo chown root:root .wifi-private bin/wifi
--- 35-fix-ssh-perms.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. "$(dirname "$0")/util.sh"
-
-chmod og-rwx .ssh .ssh/id_rsa
