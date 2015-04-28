ad1ea7e51ab59ed87376f7b680f669d6ea06c0cf
Mathias Svensson <idolf@pwnies.dk>
2015-04-28 23:03:14 +0200

    .

--- 40-check-pkgs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-. "$(dirname "$0")/util.sh"
-
-echo 'DPkg::Post-Invoke {"'$HOME'/bin/check-pkgs";};' | run sudo tee /etc/apt/apt.conf.d/99check-pkgs
-
-run sudo apt-get -y install linux-headers-$(uname -r)
-run check-pkgs
-
-
