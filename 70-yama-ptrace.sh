28bc7143e091ba9bda371b610b18145eab584dc5
Mathias Svensson <idolf@pwnies.dk>
2015-07-30 12:59:25 +0200

    Also disable ptrace-yama when the file exists

--- 70-yama-ptrace.sh
+++ 70-yama-ptrace.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 . "$(dirname "$0")/util.sh"
 
-if [ ! -e /etc/sysctl.d/10-ptrace.conf ] ; then
-    run sudo tee /etc/sysctl.d/10-ptrace.conf <<EOF
+run sudo tee /etc/sysctl.d/10-ptrace.conf <<EOF
 kernel.yama.ptrace_scope = 0
 EOF
-fi
