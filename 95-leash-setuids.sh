6a78076ec4e2cdaf90d012f13f65b2e88e1861e9
Mathias Svensson <idolf@pwnies.dk>
2015-08-03 17:00:07 +0200

    Added a few more binaries to the please-no-setuid list

--- 95-leash-setuids.sh
+++ 95-leash-setuids.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 sudo chmod -s /bin/mount
+sudo chmod -s /bin/ping6
 sudo chmod -s /bin/su
 sudo chmod -s /bin/umount
 sudo chmod -s /sbin/pam_extrausers_chkpwd
 sudo chmod -s /sbin/unix_chkpwd
+sudo chmod -s /usr/bin/X
 sudo chmod -s /usr/bin/bsd-write
 sudo chmod -s /usr/bin/chage
 sudo chmod -s /usr/bin/chfn
@@ -12,10 +14,10 @@ sudo chmod -s /usr/bin/chsh
 sudo chmod -s /usr/bin/dotlockfile
 sudo chmod -s /usr/bin/expiry
 sudo chmod -s /usr/bin/gpasswd
+sudo chmod -s /usr/bin/newgrp
 sudo chmod -s /usr/bin/passwd
 sudo chmod -s /usr/bin/ssh-agent
 sudo chmod -s /usr/bin/wall
-sudo chmod -s /usr/bin/X
 sudo chmod -s /usr/lib/emacs/24.4/x86_64-linux-gnu/movemail
 sudo chmod -s /usr/lib/libvte-2.90-9/gnome-pty-helper
 sudo chmod -s /usr/lib/openssh/ssh-keysign
