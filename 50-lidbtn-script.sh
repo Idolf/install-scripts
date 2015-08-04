031d86ad15fab7cc33372e274039f3de118422fc
Mathias Svensson <idolf@pwnies.dk>
2015-08-04 02:25:45 +0200

    Updated lid script

--- 50-lidbtn-script.sh
+++ 50-lidbtn-script.sh
@@ -5,20 +5,6 @@ prologue
 sudo tee /root/lid.sh >/dev/null <<EOFF
 #!/bin/sh
 
-if [ "$1" = "--usage" ]; then
-  cat << DONE
-You might want to do this:
-
-sudo tee /etc/acpi/events/mylid >/dev/null <<EOF
-# /etc/acpi/events/mylid
-# Called when the user closes or opens the lid
-
-event=button[ /]lid
-action=$(realpath $0)
-EOF
-DONE
-fi
-
 if grep -q closed /proc/acpi/button/lid/LID/state; then
   su - freaken -c 'DISPLAY=:0 slock' &
   sleep 1
