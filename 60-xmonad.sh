8d2aa961a073d9147a47af35ab0de537e2ea173f
Mathias Svensson <idolf@pwnies.dk>
2015-12-26 09:59:34 +0100

    Added brightness handling

--- 60-xmonad.sh
+++ 60-xmonad.sh
@@ -9,6 +9,12 @@ cd $HOME/git/xmonad
 run sudo gcc blink.c -o /usr/local/bin/blink
 run sudo chmod u+s /usr/local/bin/blink
 
+# Compile `brightness' to control screen brightness
+run sudo gcc brightness.c -o /usr/local/bin/_brightness
+run sudo chmod u+s /usr/local/bin/_brightness
+run sudo cp brightness.py /usr/local/bin/brightness
+run sudo chmod 755 /usr/local/bin/brightness
+
 run stack setup
 run stack install
 . ~/.environment
