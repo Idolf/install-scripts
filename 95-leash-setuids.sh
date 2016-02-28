851895bb574c17b76daccef86cdb5401f91d898d
Mathias Svensson <idolf@pwnies.dk>
2016-02-28 11:07:19 +0100

    better handling of setuids

--- 95-leash-setuids.sh
+++ 95-leash-setuids.sh
@@ -22,8 +22,12 @@ sudo chmod -s /usr/lib/emacs/*/x86_64-linux-gnu/movemail
 sudo chmod -s /usr/lib/libvte-*/gnome-pty-helper
 sudo chmod -s /usr/lib/openssh/ssh-keysign
 sudo chmod -s /usr/lib/pt_chown
+sudo chmod -s /usr/lib/virtualbox/VBoxSDL
+sudo chmod -s /usr/lib/virtualbox/VBoxNetAdpCtl
+sudo chmod -s /usr/lib/virtualbox/VBoxHeadless
+sudo chmod -s /usr/lib/virtualbox/VBoxNetDHCP
 sudo setcap -r /usr/bin/systemd-detect-virt 2>/dev/null || true
 sudo setcap -r /usr/lib/x86_64-linux-gnu/gstreamer*/gstreamer-*/gst-ptp-helper 2>/dev/null || true
 
-sudo find / \( -path /var/lib/docker -prune \) -o -type f -perm /6000 -ls 2>/dev/null
-sudo find / \( -path /var/lib/docker -prune \) -o -type f -perm /0111 -exec getcap {} + 2>/dev/null
+sudo find / -type f -perm /6000 -ls 2>/dev/null
+sudo find / -type f -perm /0111 -exec getcap {} + 2>/dev/null
