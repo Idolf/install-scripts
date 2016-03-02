ca39793027c463854284cb7608c2a985b0188217
Mathias Svensson <idolf@pwnies.dk>
2016-03-02 13:05:09 +0100

    Finish installing stack

--- 45-stack.sh
+++ 45-stack.sh
@@ -5,3 +5,4 @@ wget -q -O- https://s3.amazonaws.com/download.fpcomplete.com/ubuntu/fpco.key | s
 echo 'deb http://download.fpcomplete.com/ubuntu/wily stable main'|sudo tee /etc/apt/sources.list.d/fpco.list
 sudo apt-get update
 require stack
+stack setup
