a5414164e33ca1b11fe4b3c616c822553b5a389d
Mathias Svensson <idolf@pwnies.dk>
2016-03-29 19:01:00 +0200

    Use stable by default

--- 50-rust.sh
+++ 50-rust.sh
@@ -6,6 +6,6 @@ git clone --recursive https://github.com/brson/multirust && cd multirust
 git submodule update --init
 ./build.sh
 sudo ./install.sh --prefix=/opt/multirust
-/opt/multirust/bin/multirust default beta
+/opt/multirust/bin/multirust default stable
 
 epilogue
