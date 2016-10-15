17c26f5f670a99ee1bd4d83cc6a9d363a623f5b7
Mathias Svensson <idolf@pwnies.dk>
2016-10-15 14:41:48 +0200

    Update the rofi install

--- 65-rofi.sh
+++ 65-rofi.sh
@@ -2,12 +2,15 @@
 . "$(dirname "$0")/util.sh"
 prologue
 
-require autoconf automake libpango1.0-dev libxinerama-dev make libstartup-notification0-dev
+require autoconf automake libpango1.0-dev libxinerama-dev make libstartup-notification0-dev libxcb-util-dev
+require libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb-ewmh-dev libxcb-xrm-dev
+require libxcb-icccm4-dev libxcb-randr0-dev libxcb-xinerama0-dev
 # clone repo in tmp dir
 run git clone https://github.com/DaveDavenport/rofi.git .
 
 # install
 # recipe from INSTALL.md
+run git submodule update --init
 run autoreconf -i
 run mkdir build
 run cd build
