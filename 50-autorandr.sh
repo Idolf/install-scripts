26b5a40ee928004cdc1b37310667e2689e4005c7
Mathias Svensson <idolf@pwnies.dk>
2016-10-15 12:03:54 +0200

    Updated versions

--- 50-autorandr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. "$(dirname "$0")/util.sh"
-prologue
-
-run git clone git@github.com:phillipberndt/autorandr.git .
-run sudo make install
-
-epilogue
--- 50-binutils.sh
+++ 50-binutils.sh
@@ -5,14 +5,14 @@ prologue
 # requirements
 require gcc make texinfo gcc-multilib
 
-run wget http://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.gz http://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.gz.sig
-run gpg --verify binutils-2.26.tar.gz.sig || exit 1
-run tar zxfv binutils-2.26.tar.gz
+run wget http://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz http://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz.sig
+run gpg --verify binutils-2.27.tar.gz.sig || exit 1
+run tar zxfv binutils-2.27.tar.gz
 for TARGET in i386 x86_64 aarch64 alpha avr arm mips msp430 powerpc m68k sparc vax; do
   run rm -rf binutils-build
   run mkdir binutils-build
   run pushd binutils-build
-  ../binutils-2.26/configure --prefix=/opt/binutils-$TARGET \
+  ../binutils-2.27/configure --prefix=/opt/binutils-$TARGET \
     --target=$TARGET-unknown-linux-gnu --disable-static --disable-multilib \
     --disable-werror --disable-nls && \
     run make -j5
