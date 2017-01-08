aa8a3d5e4a20fd4e182e1636bfe346c790a6cac5
Mathias Svensson <idolf@pwnies.dk>
2017-01-08 21:21:25 +0100

    .

---
+++ 70-sysctl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+. "$(dirname "$0")/util.sh"
+
+sudo sed -i -e 's/use_tempaddr.*/use_tempaddr = 0/' /etc/sysctl.d/10-ipv6-privacy.conf || true
+
+echo kernel.yama.ptrace_scope = 0 | sudo tee /etc/sysctl.d/10-ptrace.conf
+echo kernel.kptr_restrict = 1 | sudo tee /etc/sysctl.d/10-kptr-restrict.conf
