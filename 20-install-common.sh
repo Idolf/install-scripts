28b082a514472765a4d320e646d7ce54f40dfc4d
Mathias Svensson <idolf@pwnies.dk>
2017-01-08 19:06:32 +0100

    .

--- 20-install-common.sh
+++ 20-install-common.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . "$(dirname "$0")/util.sh"
 
-require coreutils gcc git gnupg netcat-openbsd python openssh-client tar wget bzip2 python-yaml make
+require coreutils gcc git gnupg netcat-openbsd python openssh-client tar wget bzip2 python-yaml make nmap
