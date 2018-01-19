e7bfb74de14d90c77477bc5335c706449c9d9783
Mathias Svensson <freaken@freaken.dk>
2018-01-19 19:33:30 +0100

    Gcloud stuff

---
+++ 60-gcloud.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+. "$(dirname "$0")/util.sh"
+prologue
+
+CLOUD_SDK_REPO="cloud-sdk-yakkety"
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg > gcloud-key.gpg
+sha256sum gcloud-key.gpg | grep -q 00bd7b1fbff8cbb74f4cab874624ee36db81373b0ca891c1c35b13e762437401
+sudo apt-key add - < gcloud-key.gpg
+echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install google-cloud-sdk kubectl
+
+epilogue
--- 60-racer.sh
+++ 60-racer.sh
@@ -4,11 +4,5 @@
 prologue
 
 cargo install racer || true
-wget https://static.rust-lang.org/dist/rustc-1.9.0-src.tar.gz
-SRC=$PWD/rustc-1.9.0-src.tar.gz
-
-mkdir $HOME/.rust-src || true
-cd $HOME/.rust-src
-tar xf $SRC
 
 epilogue
