1d48452a36059943e401cdac11c1195c9083abc7
Mathias Svensson <freaken@freaken.dk>
2018-01-20 16:38:25 +0100

    Add xorg config script

---
+++ 70-x220-xorg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+. "$(dirname "$0")/util.sh"
+
+cat << EOF | sudo tee /etc/X11/xorg.conf.d/20-intel.conf
+Section "Device"
+  Identifier  "Intel Graphics"
+  Driver      "intel"
+  Option "TearFree" "true"
+EndSection
+EOF
